d2a6e68177b817bedf3a69dbacce6ed620ad1f67
Donicia <51253482+Donicia@users.noreply.github.com>
2019-09-28 21:42:57 -0500

    Add files via upload

--- Javascript-challenge/UFO-level-1/static/js/app.js
+++ Javascript-challenge/UFO-level-1/static/js/app.js
@@ -1,40 +1,47 @@
 // from data.js
 var tableData = data;
 
-// Use D3 to select the table
-var table = d3.select("table");
-
-// Use d3 to create a bootstrap striped table
-// http://getbootstrap.com/docs/3.3/css/#tables-striped
-table.attr("class", "table table-striped");
-
-// Use D3 to select the table body
-var tbody = d3.select("tbody");
-
-// YOUR CODE HERE!
-
-data.forEach(tableData => {
+// function to display UFO sightings
+function Display(UFO) {
+  var tbody = d3.select("tbody");
+  UFO.forEach((Records) => {
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
+    Object.entries(Records).forEach(([key, value]) => {
       var cell = row.append("td");
-      cell.text(value);
+      cell.html(value);
     });
   });
+};
+
+// Clean out table for filter request
+function deleteTbody() {
+  d3.select("tbody")
+    .selectAll("tr").remove()
+    .selectAll("td").remove();
+};
+
+console.log(tableData);
+Display(tableData);
 
 //***************************************************************
 
 // Select the button
 var button = d3.select("#filter-btn");
 
-button.on("click", function() {
-
+button.on("click", function(event) {
   d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime").property("value");
-  //console.log(inputElement);
-  console.log(inputElement);
   
-  var filteredTable = tableData.filter(d => d.datetime === inputElement);
-  console.log(filteredTable);
+  deleteTbody();
+  
+  var dateInput = d3.select("#datetime").property("value");
+
+// Select the input element and get the raw HTML node  
+  var filteredData = tableData.filter(UFO => 
+      UFO.datetime === dateInput.trim());
+      
+  console.log(filteredData);
+  Display(filteredData);
 });
+  
+
+
